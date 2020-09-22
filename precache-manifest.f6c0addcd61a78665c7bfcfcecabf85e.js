0465a879ae2483da8322614a9e902ab8717c81de
bobihariadi <bobihariadi@gmial.com>
2020-09-22 09:28:15 +0700

    Updates

---
+++ precache-manifest.f6c0addcd61a78665c7bfcfcecabf85e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "14430bfc275506c74080",
+    "url": "/reactjs-my-app/static/css/main.67f3034f.chunk.css"
+  },
+  {
+    "revision": "14430bfc275506c74080",
+    "url": "/reactjs-my-app/static/js/main.8c3a38ef.chunk.js"
+  },
+  {
+    "revision": "0fee419e1b6eb90d310a",
+    "url": "/reactjs-my-app/static/js/runtime~main.0e6e67ad.js"
+  },
+  {
+    "revision": "f717189db42a5946251b",
+    "url": "/reactjs-my-app/static/js/2.af189cfa.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactjs-my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c1a8f23ec50ab26ec745a25ae1c2b35e",
+    "url": "/reactjs-my-app/index.html"
+  }
+];
